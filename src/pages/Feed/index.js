54f5cefda8c31409859fc9b55699c42fd556975d
elcachorroloko <raitoyagami2013@live.com>
2019-08-27 21:06:53 -0400

    first commit

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, FlatList } from 'react-native';
+
+import LazyImage from '../../components/LazyImage';
+
+import { Post, Avatar, Header, Description, Name, Loading } from './styles';
+
+export default function Feed() {
+    const [ feed, setFeed ] = useState([]);
+    const [ page, setPage ] = useState(1);
+    const [ total, setTotal ] = useState(0);
+    const [ loading, setLoading ] = useState(false);
+    const [ refreshing, setRefreshing ] = useState(false);
+    const [ viewable, setViewable ] = useState([]);
+
+    async function loadPage(pageNumber = page, shouldRefresh = false) {
+        if(total && pageNumber > total) return;
+
+        setLoading(true);
+
+        const response = await fetch(
+            `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+        );
+
+        const data = await response.json();
+        const totalItems = response.headers.get('X-Total-Count')
+
+        setTotal(Math.floor(totalItems / 5));
+        setFeed( shouldRefresh ? data : [...feed, ...data]);
+        setPage(pageNumber + 1);
+
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadPage();
+    }, []);
+
+    async function refreshList() {
+        setRefreshing(true);
+
+        await loadPage(1, true);
+
+        setRefreshing(false);
+    }
+
+    const handleViewableChanged = useCallback(({ changed }) => {
+        setViewable(changed.map(({ item }) => item.id));
+    }, []);
+
+    return (
+        <View>
+            <FlatList
+                data={feed}
+                keyExtractor={post => String(post.id)}
+                onEndReached={() => loadPage()}
+                onEndReachedThreshold={0.1}
+                onRefresh={refreshList}
+                viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
+                refreshing={refreshing}
+                onViewableItemsChanged={handleViewableChanged}
+                ListFooterComponent={loading && <Loading/>}
+                renderItem={({ item }) => (
+                    <Post>
+                        <Header>
+                            <Avatar source={{ uri: item.author.avatar }}/>
+                            <Name>{item.author.name}</Name>
+                        </Header>
+
+                        <LazyImage 
+                            shouldLoad={viewable.includes(item.id)}
+                            aspectRatio={item.aspectRatio}
+                            source={{ uri: item.image }}
+                            smallSource={{ uri: item.small }}
+                        />
+
+                        <Description>
+                            <Name>{item.author.name}</Name> {item.description}
+                        </Description>
+                    </Post>
+                )}
+            />
+        </View>
+    );
+}
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { Image } from 'react-native';
+
+import Feed from './pages/Feed';
+
+import logo from './assets/instagram.png'
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Feed
+    }, {
+        headerLayoutPreset: 'center',
+        defaultNavigationOptions: {
+            headerTitle: <Image source={logo}/>,
+            headerStyle: {
+                backgroundColor:'#f5f5f5'
+            }
+        }
+    })
+)
+
+export default Routes;
